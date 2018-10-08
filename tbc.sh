bb5a69b4c97820f29b5d3b6604eee93a708223d7
Delaram Ghoreishi <delaramq@gmail.com>
2018-10-08 10:40:53 -0400

    Update tbc.sh

--- tbc.sh
+++ tbc.sh
@@ -1,1 +1,2 @@
 for ((i=2; i<10; i++)); do ext=`printf "groupfile_%02i" $i`; sed -i -e 's/runGPU/runGPU_amber18/g' $ext; done
+for ((i=0; i<10; i++)); do ext=`printf "groupfile_%02i" $i`; Ext=`printf "groupfile_nstep_%02i" $i`; cp $ext $Ext; sed -i -e 's/runGPU_amber18/runGPU_nstep/g' $Ext; done
